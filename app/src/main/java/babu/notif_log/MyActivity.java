19ad99c18ad7cc5bb059a738f5e927f52abe43eb
Junaid Babu <junaidvayoli@gmail.com>
2014-07-15 10:12:47 +0900

    This much will read the currents

--- app/src/main/java/babu/notif_log/MyActivity.java
+++ app/src/main/java/babu/notif_log/MyActivity.java
@@ -1,19 +1,70 @@
 package babu.notif_log;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Date;
 
 
 public class MyActivity extends Activity {
 
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        t1 = (TextView) findViewById(R.id.text);
+      //  Uri allCalls = Uri.parse("content://call_log/calls");
+       // Cursor c = managedQuery(allCalls, null, null, null, null);
+      //  Log.e("C", c.toString());
+       // String num= c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
+        t1.setText(getCallDetails());
     }
+    private String getCallDetails() {
 
+        StringBuffer sb = new StringBuffer();
+        Cursor managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null,
+                null, null, null);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.CACHED_NAME);
+        sb.append("Call Details :");
+        while (managedCursor.moveToNext()) {
+            String phNumber = managedCursor.getString(number);
+            String callType = managedCursor.getString(type);
+            String callDate = managedCursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = managedCursor.getString(duration);
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+                    + dir + " \nCall Date:--- " + callDayTime
+                    + " \nCall duration in sec :--- " + callDuration);
+            sb.append("\n----------------------------------");
+        }
+        managedCursor.close();
+        return sb.toString();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
